88cc243da398cce75165d3c306fb194e0ebd6245
gamersep <73775681+gamersep@users.noreply.github.com>
2021-07-17 15:59:45 +0530

    Update sketch.js

--- project37-main/sketch.js
+++ project37-main/sketch.js
@@ -1,4 +1,8 @@
 var canvas, backgroundImage;
+var canvas, backgroundImage;
+var gameState=0;
+var playerCount=0;
+var allPlayers;
 
 
 
@@ -24,3 +28,29 @@ function draw(){
   
   
 }
+var distance=0
+var form,player,game
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  
+  game=new Game ()
+  game.getState ()
+  game.start()
+  
+  
+  
+}
+
+
+function draw(){
+if(playerCount===4){
+  game.update(1)
+}  
+  if(gameState===1){
+    clear ()
+    game.play()
+  }
+  
+}
+
